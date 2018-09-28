403e80bfd2c26fe171ad03dbf66393c3a990838d
alexdejko <dag@brest.beltelecom.by>
2018-09-28 11:01:43 +0300

    7-th commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+	let num = number
+	let tmp = num;
+	let count = 0;
+	let pow = 1;
+	while (tmp>=1){
+		tmp = Math.floor(num/Math.pow(5,pow));
+		count = count + tmp;
+		pow++;
+	}
+	return count;
 }
